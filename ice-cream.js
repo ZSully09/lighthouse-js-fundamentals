cb0506cdcbe5c8d9075e7cabce134657a53c43e9
ZSully09 <zsullivan93@gmail.com>
2019-09-27 07:03:37 +0000

    ice-cream array example

---
+++ ice-cream.js
@@ -0,0 +1,23 @@
+// Do not modify these first two lines
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry"
+];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+// Add "root beer" & print
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// Console.log the first flavour in the array
+console.log(iceCreamFlavours[0]);
+
+// Console.log the last flavour in the array
+console.log(iceCreamFlavours[5]);
+
+// Console.log the total number of flavours
+console.log(iceCreamFlavours.length);
